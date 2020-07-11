203e40f48e32a130f82ce7246b12a7643f509e45
mbkunal <mb.kunal@gmail.com>
2020-07-11 11:39:59 +0530

    PieCart Optimized
    Added better visualization for small data in piecharts

--- app/src/main/java/com/kmb/budget/ShowChart.java
+++ app/src/main/java/com/kmb/budget/ShowChart.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
@@ -23,6 +24,7 @@ import android.view.View;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.github.mikephil.charting.components.Legend.LegendPosition.ABOVE_CHART_CENTER;
 import static com.github.mikephil.charting.utils.ColorTemplate.createColors;
 
 public class ShowChart extends AppCompatActivity {
@@ -49,25 +51,61 @@ public class ShowChart extends AppCompatActivity {
     public static void showChart(List<CategorySum> list){
 
         ArrayList balance = new ArrayList();
+        ArrayList balance2 = new ArrayList();
+        ArrayList balance3 = new ArrayList();
+
+
+        //Below lines are just to make sure that all the data is visible.
+        int aSum = 0;
+        int i = 0;
+        float avg = 0;
         for(CategorySum cs : list){
             if(cs.getType().toUpperCase().equals("Investment".toUpperCase()) || cs.getType().toUpperCase().equals("Source".toUpperCase()) || cs.getType().toUpperCase().equals("BTF".toUpperCase())){
-                balance.add(new PieEntry(Integer.parseInt(cs.getBalance()),cs.getCategoryName()));
+                if( Integer.parseInt(cs.getBalance()) > 0) {
+                    aSum += Integer.parseInt(cs.getBalance());
+                    i++;
+                }
             }
-        }/*
-        balance.add(new PieEntry(10f,"Mutual Fund"));
-        balance.add(new PieEntry(20f,"BTF"));
-        balance.add(new PieEntry(30f,"Equity"));
-        balance.add(new PieEntry(40f,"Cash"));
-        balance.add(new PieEntry(50f,"Bank"));
-        balance.add(new PieEntry(60f,"FD"));*/
-        PieDataSet dataSet = new PieDataSet(balance, "Balance");
+        }
+        try {
+            avg = aSum / i;
+        }catch (Exception e){
+            avg = 0;
+            e.printStackTrace();
+        }
+        for(CategorySum cs : list){
+            if(cs.getType().toUpperCase().equals("Investment".toUpperCase()) || cs.getType().toUpperCase().equals("Source".toUpperCase()) || cs.getType().toUpperCase().equals("BTF".toUpperCase())){
+                if( Integer.parseInt(cs.getBalance()) > 0) {
+                    if(Float.parseFloat((cs.getBalance())) >  avg){
+                        balance.add(new PieEntry(Float.parseFloat(cs.getBalance()), cs.getCategoryName()));
+                    }else{
+                        balance2.add(new PieEntry(Float.parseFloat(cs.getBalance()), cs.getCategoryName()));
+                    }
+
+                    Log.e("cs.getCategoryName()", String.valueOf(Float.parseFloat(cs.getBalance())));
+                }
+            }
+        }
+        if (balance.size() > balance2.size()){
+            while(balance2.size()>0) {
+                balance3.add(balance.remove(0));
+                balance3.add(balance2.remove(0));
+            }
+            balance3.addAll(balance);
+        }else
+        {
+            while(balance.size()>0) {
+                balance3.add(balance2.remove(0));
+                balance3.add(balance.remove(0));
+            }
+            balance3.addAll(balance2);
+        }
+        PieDataSet dataSet = new PieDataSet(balance3, "Balance");
 
         PieData data = new PieData(dataSet);
         pChart.setData(data);
         dataSet.setColors(colors);
         pChart.animateXY(1000, 1000);
-
-
     }
 
 }
