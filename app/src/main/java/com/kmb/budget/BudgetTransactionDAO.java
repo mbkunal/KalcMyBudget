a5f68be7cab1242281a46ab48f6e50e94a7ac5bc
mbkunal <mb.kunal@gmail.com>
2020-10-02 20:13:37 +0530

    1 step to manage budget function -- added table for budget transactions --
    Signed-off-by: mbkunal <mb.kunal@gmail.com>

---
+++ app/src/main/java/com/kmb/budget/BudgetTransactionDAO.java
@@ -0,0 +1,49 @@
+package com.kmb.budget;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface BudgetTransactionDAO {
+
+    @Query("SELECT * FROM budgetTransactions ORDER BY transactionDate")
+    List<BudgetTransactionModal> getAllTransactions();
+
+    @Query("Select * from budgetTransactions where _id= :transactionId")
+    BudgetTransactionModal getTransaction(long transactionId);
+
+    @Query("SELECT * FROM budgetTransactions WHERE transactionDate BETWEEN :from AND :to")
+    List<BudgetTransactionModal> getTransactions(long from, long to);
+
+    @Query("SELECT * FROM budgetTransactions WHERE transactionDate BETWEEN :from AND :to AND ( toId = :categoryId or fromId= :categoryId ) ORDER BY transactionDate")
+    List<BudgetTransactionModal> getTransactions(long from, long to, long categoryId);
+
+    @Query("Select COUNT(*) from budgetTransactions")
+    int countTransactions();
+
+    @Query("Select * from budgetTransactions where toId = :toCategoryId")
+    List<BudgetTransactionModal> getCreditTransaction(long toCategoryId);
+
+    @Query("Select * from budgetTransactions where fromId = :fromCategoryId")
+    List<BudgetTransactionModal> getDebitTransaction(long fromCategoryId);
+
+    @Query("Delete from budgetTransactions where _id= :transactionId")
+    void deleteTransactionById(Long transactionId);
+
+    @Delete
+    void delete(BudgetTransactionModal transaction) ;
+
+    @Insert
+    void insert(BudgetTransactionModal transaction);
+
+    @Update
+    void update(BudgetTransactionModal transaction);
+
+    @Query("Select * from budgetTransactions where toId = :categoryId or fromId= :categoryId ORDER BY transactionDate")
+    List<BudgetTransactionModal> getAllTransactionsByCategory(Long categoryId);
+}
--- app/src/main/java/com/kmb/budget/MainDatabase.java
+++ app/src/main/java/com/kmb/budget/MainDatabase.java
@@ -6,9 +6,11 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 
-@Database(entities = {TransactionModal.class,CategoryModal.class},version = 1)
+@Database(entities = {TransactionModal.class,CategoryModal.class,BudgetTransactionModal.class},version = 2)
 @TypeConverters({Converters.class})
 public abstract class MainDatabase extends RoomDatabase {
 
@@ -16,14 +18,36 @@ public abstract class MainDatabase extends RoomDatabase {
 
     public abstract TransactionDAO transactionDAO();
     public abstract CategoryDAO categoryDAO();
-
+    public abstract BudgetTransactionDAO budgetTransactionDAO();
     public static MainDatabase getMainDatabase(Context context) {
+
+        Migration MIGRATION_1_2 = new Migration(1, 2) {
+            @Override
+            public void migrate(SupportSQLiteDatabase database) {
+                database.execSQL("CREATE TABLE `budgetTransactions` (`_id` INTEGER NOT NULL, "
+                        + "`toId` INTEGER NOT NULL,"
+                        + "`fromId` INTEGER NOT NULL,"
+                        + "`amount` INTEGER NOT NULL,"
+                        + "`comment` TEXT,"
+                        + "`transactionDate` INTEGER,"
+                        + "`createDate` INTEGER,"
+                        + " PRIMARY KEY(`_id`))");
+            }
+        };
+
         if (INSTANCE == null) {
             INSTANCE =
-                    Room.databaseBuilder(context.getApplicationContext(), MainDatabase.class, "main-database").build();
+                    Room.databaseBuilder(context.getApplicationContext(), MainDatabase.class, "main-database").addMigrations(MIGRATION_1_2).build();
+
+
+
         }
         return INSTANCE;
+
+
     }
+
+
 
     public static void destroyInstance() {
         INSTANCE = null;
